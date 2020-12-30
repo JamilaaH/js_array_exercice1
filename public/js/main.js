7eb31a6c74ac21d7d7868f355511d6b8b7688a2d
Jamila <jaja_mimi@hotmail.com>
2020-12-30 14:37:18 +0100

    tableau

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let molengeek = ["Coding 15", "Coding 16", "Coding 17"];
+console.log(molengeek);
+
+console.log(molengeek.length);
+
+console.log(molengeek[0]);
+console.log(molengeek[1]);
+console.log(molengeek[2]);
